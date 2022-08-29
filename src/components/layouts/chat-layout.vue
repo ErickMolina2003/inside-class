b9fa5fc84fd6f37f710781f849f1e7d84efe3a8d
erick molina <eralejo2003@gmail.com>
2022-08-29 16:00:31 -0600

    IC-21 emojis container

--- src/components/layouts/chat-layout.vue
+++ src/components/layouts/chat-layout.vue
@@ -34,6 +34,7 @@
 
     <v-footer color="var(--v-primary-lighten2)" app height="72" inset>
       <v-text-field
+        v-model="input"
         background-color="var(--v-primary-darken3)"
         append-icon="mdi-send"
         dark
@@ -44,7 +45,36 @@
         rounded
         solo
       ></v-text-field>
-      <v-icon class="white--text ml-2">mdi-emoticon</v-icon>
+      <div class="wrapper mr-3">
+        <emoji-picker @emoji="insert">
+          <button
+            class="emoji-invoker"
+            slot="emoji-invoker"
+            slot-scope="{ events: { click: clickEvent } }"
+            @click.stop="clickEvent"
+          >
+            <v-icon class="white--text">mdi-emoticon</v-icon>
+          </button>
+          <div slot="emoji-picker" slot-scope="{ emojis, insert }">
+            <div class="emoji-picker">
+              <div>
+                <div v-for="(emojiGroup, category) in emojis" :key="category">
+                  <h5>{{ category }}</h5>
+                  <div class="emojis">
+                    <span
+                      v-for="(emoji, emojiName) in emojiGroup"
+                      :key="emojiName"
+                      @click="insert(emoji)"
+                      :title="emojiName"
+                      >{{ emoji }}</span
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </emoji-picker>
+      </div>
     </v-footer>
   </v-app>
 </template>
@@ -56,6 +86,7 @@ import SecondarySideBar from "@/components/side-bars/secondary-side-bar.vue";
 import RightSideBar from "@/components/side-bars/right-side-bar.vue";
 import ChattingComponent from "@/components/chats/chatting-component.vue";
 import MainSideBar from "@/components/layouts/main-layout.vue";
+import EmojiPicker from "vue-emoji-picker";
 
 @Component({
   name: "ChatLayout",
@@ -64,15 +95,81 @@ import MainSideBar from "@/components/layouts/main-layout.vue";
     RightSideBar,
     ChattingComponent,
     MainSideBar,
+    EmojiPicker,
   },
 })
 export default class ChatLayout extends Vue {
   drawer = null;
+
+  input = "";
+
+  insert(emoji: string) {
+    this.input += emoji;
+  }
 }
 </script>
 
 <style scoped>
 .main-wrap {
   background-color: var(--v-primary-lighten2);
+}
+
+.emoji-invoker {
+  position: absolute;
+  bottom: 1.7rem;
+  right: 0rem;
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: 50%;
+  cursor: pointer;
+  transition: all 0.2s;
+  padding: 0;
+  background: transparent;
+  border: 0;
+}
+.emoji-invoker:hover {
+  transform: scale(1.1);
+}
+
+.emoji-picker {
+  background-color: var(--v-primarybright-base);
+  position: fixed;
+  bottom: 3.5rem;
+  right: 17rem;
+  max-width: 25rem;
+  max-height: 10rem;
+  border-radius: 20px;
+  padding: 6px;
+  overflow: scroll;
+}
+
+.emoji-picker::-webkit-scrollbar {
+  display: none;
+}
+
+.emoji-picker h5 {
+  margin-bottom: 0;
+  color: #b1b1b1;
+  text-transform: uppercase;
+  font-size: 0.8rem;
+  cursor: default;
+}
+.emoji-picker .emojis {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.emoji-picker .emojis:after {
+  content: "";
+  flex: auto;
+}
+.emoji-picker .emojis span {
+  padding: 0.2rem;
+  cursor: pointer;
+  border-radius: 5px;
+}
+.emoji-picker .emojis span:hover {
+  background: #ececec;
+  cursor: pointer;
 }
 </style>
