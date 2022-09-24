ab938f8b0b1cd10a1e8ad932001192fc78a8fa3b
erick molina <eralejo2003@gmail.com>
2022-09-23 19:37:56 -0600

    IC-30 User login & Vuex

--- src/views/chat-card.vue
+++ src/views/chat-card.vue
@@ -2,18 +2,18 @@
   <v-card class="chat-card mx-auto my-6" max-width="227" max-height="307">
     <v-img height="124" src="../assets/chats-img/chat-default.svg"></v-img>
 
-    <h4 class="white--text text-center mt-3">IS501-BASE DE DATOS I</h4>
+    <h4 class="white--text text-center mt-3">{{ chat.title }}</h4>
 
     <v-card-text class="pt-0">
-      <h5 class="white--text text-center">IS-IQ-IE</h5>
+      <h5 class="white--text text-center">{{ chat.code }}</h5>
     </v-card-text>
 
     <v-row justify="center" class="pt-2">
       <v-col lg="2" md="2" cols="2">
         <v-icon class="white--text">mdi-account-group</v-icon>
       </v-col>
-      <v-col lg="8" md="8" cols="8">
-        <h5 class="white--text">100 Participantes</h5>
+      <v-col lg="8" md="8" cols="8" v-if="chat.numberMembers">
+        <h5 class="white--text">{{ chat.numberMembers }} Participantes</h5>
       </v-col>
     </v-row>
 
@@ -26,12 +26,15 @@
 </template>
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Prop } from "vue-property-decorator";
+import { Chat } from "@/store/models/chats";
 
 @Component({
   name: "ChatCard",
 })
-export default class ChatCard extends Vue {}
+export default class ChatCard extends Vue {
+  @Prop({ default: [] }) chat!: Chat;
+}
 </script>
 
 <style scoped>
